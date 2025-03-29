347955fdaa036fa6b2c8df969efe8d309c97f459
robinjo1776 <robinjo1776@gmail.com>
2025-03-29 09:16:49 +0000

    Carrier module complete

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Shipment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ShipmentController extends Controller
 {
@@ -15,17 +17,24 @@ class ShipmentController extends Controller
         $this->shipment = new Shipment();
     }
 
-    // Get all shipments
+
     public function index()
     {
         return response()->json($this->shipment->orderBy('created_at', 'desc')->get());
     }
-    // Create a new shipment
+
     public function store(Request $request)
     {
-        $shipmentData = $request->all();
+        $validatedData = $this->validateShipment($request);
 
-        return $this->shipment->create($shipmentData);
+        if ($validatedData->fails()) {
+            return response()->json(['errors' => $validatedData->errors()], 422);
+        }
+
+        $shipmentData = $validatedData->validated();
+        $shipment = $this->shipment->create($shipmentData);
+
+        return response()->json($shipment, 201);
     }
 
     // Get a single shipment by ID
@@ -39,26 +48,41 @@ class ShipmentController extends Controller
     {
         $shipment = $this->shipment->find($id);
 
-        // If the lead is not found, return a 404 response with an error message
         if (!$shipment) {
             return response()->json(['error' => 'Shipment not found'], 404);
         }
 
-        // Get the data to update from the request
-        $shipmentData = $request->all();
+        $validatedData = $this->validateShipment($request, $shipment->id);
 
-        // Perform the update
-        $shipment->update($shipmentData);
+        if ($validatedData->fails()) {
+            return response()->json(['errors' => $validatedData->errors()], 422);
+        }
 
-        // Return the updated lead
+        $shipment->update($validatedData->validated());
+
         return response()->json($shipment);
     }
 
     // Delete a shipment by ID
-    public function destroy($id)
+    public function destroy(string $id)
     {
-        $shipment = Shipment::findOrFail($id);
-        $shipment->delete();
-        return response()->json(null, 204);
+        $shipment = $this->shipment->find($id);
+        return $shipment->delete();
+    }
+
+    private function validateShipment(Request $request, $id = null)
+    {
+        return Validator::make($request->all(), [
+            'ship_load_date' => 'nullable|date',
+            'ship_ftl_ltl' => ['nullable', 'string', Rule::in(["FTL", "LTL"])],
+            'ship_pickup_location' => 'nullable|string|max:150|regex:/^[a-zA-Z0-9\s.,\'\-]*$/',
+            'ship_delivery_location' => 'nullable|string|max:150|regex:/^[a-zA-Z0-9\s.,\'\-]*$/',
+            'ship_driver' => 'nullable|string|max:150|regex:/^[a-zA-Z0-9\s.,\'\-]*$/',
+            'ship_weight' => 'nullable|numeric|min:0|max:999999',
+            'ship_equipment' => 'nullable|string|max:150|regex:/^[a-zA-Z0-9\s.,\'\-]*$/',
+            'ship_price' => 'nullable|numeric|min:0|max:999999',
+            'ship_notes' => 'nullable|string|max:500|regex:/^[a-zA-Z0-9\s.,\'\-]*$/',
+            'ship_tarp' => 'boolean'
+        ]);
     }
 }
