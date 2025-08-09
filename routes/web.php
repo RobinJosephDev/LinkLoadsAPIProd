4a434dec985a93a565025d8b0839b91e5c713716
robinjo1776 <robinjo1776@gmail.com>
2025-08-09 01:46:58 +0000

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
-use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    try {
-        throw new Exception('Try message');
-    } catch (Exception $e) {
-        Debugbar::addException($e);
-    }
-    return view('welcome');
-});
+// Serve React from Laravel
+Route::get('/{any}', function () {
+    return File::get(public_path('index.html'));
+})->where('any', '.*');
+
