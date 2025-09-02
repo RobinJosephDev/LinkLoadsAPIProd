0840f0e25f5ae1abaca5624f01e948d74f766e3a
robinjo1776 <robinjo1776@gmail.com>
2025-09-02 10:16:45 +0000

    Role based access control completed

---
+++ public/assets/LeadFollowupPage-BEylFxRR.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CZ9Kvx3V.js";import{L as r}from"./LeadFollowupTable-C0zkaLPb.js";import"./Modal-CC9O_okn.js";import"./EyeOutlined-D8Ivf9Pr.js";import"./Pagination-O6ZxpL-l.js";import"./purify.es-Ci5xwkH_.js";/* empty css             */import"./useGoogleAutocomplete-BVymW36F.js";const d=()=>o.jsx("div",{children:o.jsx(r,{})});export{d as default};
