0198dd2425c27bdd0bc897eef09d8c4805dd6c94
Robin Joseph <robinjo1776@gmail.com>
2026-01-29 11:59:48 +0530

    cors updated

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'admin-ui-linux-riyu1hp8r-robinjo1776-5d95d5ba.vercel.app',
+        'https://admin-ui-linux-riyu1hp8r-robinjo1776-5d95d5ba.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
